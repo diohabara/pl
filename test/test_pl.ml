6a28af4e8b0b6a9ff2b83e7adc7b5a4aef3913b3
TKadoi <diohabara@gmail.com>
2022-03-20 17:29:44 +0900

    test: add a test case

--- test/test_pl.ml
+++ test/test_pl.ml
@@ -3,5 +3,7 @@ open Pl.Interp
 
 let%expect_test "integer" =
   interp "11" |> print_endline;
-  [%expect {| 11 |}]
+  [%expect {| 11 |}];
+  interp "-22" |> print_endline;
+  [%expect {| -22 |}]
 ;;
